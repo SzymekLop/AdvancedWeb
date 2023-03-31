b489b7a95559d3c34fb8059729fc373c7c7e50fe
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-03-31 16:02:46 +0200

    initial frontend commit

---
+++ library-frontend/src/Authors.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="app">
+        <div id="page-header" class="content-box shadow-box">
+            <h1 class="page-header-item"><a class="page-button" href="/">Book Service</a></h1>
+            <a class="page-button button shadow-box" href="books">Books</a>
+            <a class="page-button button shadow-box btn-active" href="authors">Authors</a>
+        </div>
+        <div id="page-content" class="content-box shadow-box">
+            <adding-author-form @add:author="addAuthor"></adding-author-form>
+            <authors-table :authorsSource="authors" @onDelete="deleteAuthor" @onUpdate="updateAuthor"></authors-table>
+        </div>
+    </div>
+</template>
+
+<script>
+import AddingAuthorForm from './components/AddingAuthorForm.vue'
+import AuthorsTable from './components/AuthorsTable.vue'
+import axios from "axios"
+
+export default {
+    name: 'App',
+    components: {
+        AddingAuthorForm,
+        AuthorsTable,
+    },
+    data() {
+        return {
+            authors: [],
+        }
+    },
+    methods: {
+        addAuthor(author) {
+            axios.post('http://localhost:8080/authors/add', {
+                    firstName: author.firstName,
+                    lastName: author.lastName
+                })
+                .then(() => { this.getAuthors() })
+                .catch(e => alert(e))
+        },
+
+        getAuthors() {
+            axios.get('http://localhost:8080/authors/get').then(data => { this.authors = data.data }).catch(e => alert(e))
+        },
+
+        deleteAuthor(id) {
+            axios.delete(`http://localhost:8080/authors/delete/${id}`).then(() => { this.getAuthors() }).catch(e => alert(e))
+        },
+
+        updateAuthor(author) {
+            axios.put(`http://localhost:8080/books/update/${author.id}`, {
+                firstName: author.firstName,
+                lastName: author.lastName
+            }).then(() => { this.getAuthors() }).catch(e => alert(e))
+        },
+    },
+    mounted() {
+        this.getAuthors()
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ library-frontend/src/Books.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <div id="page-header" class="content-box shadow-box">
+      <h1 class="page-header-item"><a class="page-button" href="/">Book Service</a></h1>
+      <a class="page-button button shadow-box btn-active" href="books">Books</a>
+      <a class="page-button button shadow-box" href="authors">Authors</a>
+    </div>
+    <div id="page-content" class="content-box shadow-box">
+      <adding-book-form @add:book="addBook" :authorsSource="authors"></adding-book-form>
+      <books-table @onDelete="deleteBook" :booksSource="books" @onUpdate="updateBook"></books-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import AddingBookForm from './components/AddingBookForm.vue'
+import BooksTable from './components/BooksTable.vue'
+import axios from "axios"
+
+export default {
+  name: 'App',
+  components: {
+    AddingBookForm,
+    BooksTable,
+  },
+  data(){
+    return {
+      books: [],
+      authors: [],
+    }
+  }
+  ,
+  methods:{
+    addBook(book){
+      axios.post('http://localhost:8080/books/add', {
+        title: book.title,
+        author: book.author,
+        pages: book.pages
+      })
+      .then(() => { this.getBooks()})
+      .catch(e => alert(e))      
+    },
+
+    getBooks(){
+        axios.get('http://localhost:8080/books/get').then(data => {this.books = data.data}).catch(e => alert(e))
+    },
+
+    getAuthors(){
+      axios.get('http://localhost:8080/authors/get').then(data => {this.authors = data.data}).catch(e => alert(e))
+    },
+
+    deleteBook(id){
+      axios.delete(`http://localhost:8080/books/delete/${id}`).then(() => {this.getBooks()}).catch(e => alert(e))
+    },
+
+    updateBook(book){
+      axios.put(`http://localhost:8080/books/update/${book.id}`,
+      {
+        title: book.title,
+        author: book.author,
+        pages: book.pages
+      }).then(() => {this.getBooks()}).catch(e => alert(e))
+    },    
+  },
+  mounted(){
+    this.getBooks(),
+    this.getAuthors()
+  }
+}
+</script>
+
+<style>
+
+
+</style>
